35079ac5294b6bcdcda334a92634cfd422b6f717
ASUS <ASUS@PC-FORM-04>
2024-05-06 19:04:07 +0000

    mon premier commit

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MyMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+
+
+
+    }
+}
